7de4ebd7bbb478afaf545ffeb98859be9356fd8e
temitope <lemikanemmanuel@gmail.com>
2022-05-12 13:02:01 +0100

    Upperlink Test

---
+++ Upperlink_test.php
@@ -0,0 +1,24 @@
+<?php
+function calc($a, $b){
+    $array = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+    $arrCount = count($array);
+
+
+    if($a < 0 || $b < 0){
+        echo -1;
+        return;
+    }elseif(($a > $b) || ($a > $arrCount || $b > $arrCount)){
+        echo 0;
+        return;
+    }elseif(($a == $array[$a]) && ($b > $arrCount)){
+        $newArr = array_slice($array, $a);
+        $sum = array_sum($newArr);
+    }else {
+        $newArr = array_slice($array, $a, $b);
+        $sum = array_sum($newArr);
+    }
+}
+
+calc(4,5)
+
+?>
